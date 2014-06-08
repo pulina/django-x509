e6e11b16cf64f1b2ea2040179ac59ffd661f1b17
pulina <sk.boski@gmail.com>
2014-06-08 16:06:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,31 @@
-from __future__ import unicode_literals
-from os.path import abspath, dirname, join
+#from __future__ import unicode_literals
+#from os.path import abspath, dirname, join
 from setuptools import setup
 
 
-def read_relative_file(filename):
-    """Returns contents of the given file, whose path is supposed relative
-    to this module."""
-    with open(join(dirname(abspath(__file__)), filename)) as f:
-        return f.read()
+#def read_relative_file(filename):
+ #   """Returns contents of the given file, whose path is supposed relative
+##    to this module."""
+#    with open(join(dirname(abspath(__file__)), filename)) as f:
+#        return f.read()
 
 
-if __name__ == '__main__':  # ``import setup`` doesn't trigger setup().
-    setup(
-        name='django-x509',
-        version=read_relative_file('VERSION').strip(),
-        description="Let you be behind a nginx configuration an get x509 auth credentials",
-        packages=['x509'],
-        include_package_data=True,
-        zip_safe=False,
-        install_requires=('setuptools', 'flask', 'django-uuidfield', 'pyOpenSSL')
-    )
+#if __name__ == '__main__':  # ``import setup`` doesn't trigger setup().
+#    setup(
+        
+        
+#    )
+
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+      name='django-x509',
+      version='1.0',
+      description='x509',
+      author='Pulina',
+      author_email='no@mail.com',
+      url='https://github.com/pulina/django-x509/',
+      packages=['setuptools', 'flask', 'django-uuidfield', 'pyOpenSSL'],
+     )
