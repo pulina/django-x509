82a721db80376c7897866c9d7d4310155b26c454
pulina <sk.boski@gmail.com>
2014-06-08 15:59:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from os.path import abspath, dirname, join
 from setuptools import setup
@@ -16,20 +15,7 @@ if __name__ == '__main__':  # ``import setup`` doesn't trigger setup().
         name='django-x509',
         version=read_relative_file('VERSION').strip(),
         description="Let you be behind a nginx configuration an get x509 auth credentials",
-        long_description=read_relative_file('README.rst'),
-        classifiers=['Development Status :: 4 - Beta',
-                     'Environment :: Web Environment',
-                     'Intended Audience :: Developers',
-                     'License :: OSI Approved :: MIT License',
-                     'Programming Language :: Python',
-                     'Programming Language :: Python :: 2.7',
-                     'Programming Language :: Python :: 3'],
-        keywords='django wsgi x509 auth',
-        author='Rémy Hubscher',
-        author_email='hubscher.remy@gmail.com',
-        url='https://github.com/novapost/django-x509',
-        license='MIT Licence',
-        packages=['x509'],
+        packages='x509',
         include_package_data=True,
         zip_safe=False,
         install_requires=('setuptools', 'flask', 'django-uuidfield', 'pyOpenSSL')
