be6845146df9a7597985d0c93492fdf7b1359a49
pulina <sk.boski@gmail.com>
2014-06-08 16:19:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,40 @@
-#!/usr/bin/env python
+from setuptools import setup, find_packages
+import os
 
-from distutils.core import setup
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+extensions_dir = 'x509'
 
-setup(
-      name='django-x509',
-      version='1.0',
-      description='x509',
-      author='Pulina',
-      author_email='no@mail.com',
-      url='https://github.com/pulina/django-x509/',
-      packages=['setuptools', 'flask', 'django-uuidfield', 'pyOpenSSL'],
-     )
+if __name__ == '__main__':  # ``import setup`` doesn't trigger setup().
+    setup(
+        name='django-x509',
+        version='1.0',
+        description="Let you be behind a nginx configuration an get "
+                    "x509 auth credentials",
+        long_description="",
+        classifiers=['Development Status :: 4 - Beta',
+                     'Environment :: Web Environment',
+                     'Intended Audience :: Developers',
+                     'License :: OSI Approved :: MIT License',
+                     'Programming Language :: Python',
+                     'Programming Language :: Python :: 2.7',
+                     'Programming Language :: Python :: 3'],
+        keywords='django wsgi x509 auth',
+        author='Remy Hubscher',
+        author_email='hubscher.remy@gmail.com',
+        url='https://github.com/novapost/django-x509',
+        license='MIT Licence',
+        packages=find_packages(),
+        include_package_data=True,
+        zip_safe=False,
+        install_requires=['setuptools>=1.1.6', 'flask', 'django-uuidfield',
+                          'pyOpenSSL'],
+        entry_points={
+            'console_scripts': [
+            'test_app = x509.test_app:main',
+            ]
+        }
+    )
+                    
