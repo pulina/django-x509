4cac7b69d088ccef01914f043414becb04559347
pulina <sk.boski@gmail.com>
2014-06-08 16:07:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,3 @@
-#from __future__ import unicode_literals
-#from os.path import abspath, dirname, join
-from setuptools import setup
-
-
-#def read_relative_file(filename):
- #   """Returns contents of the given file, whose path is supposed relative
-##    to this module."""
-#    with open(join(dirname(abspath(__file__)), filename)) as f:
-#        return f.read()
-
-
-#if __name__ == '__main__':  # ``import setup`` doesn't trigger setup().
-#    setup(
-        
-        
-#    )
-
 #!/usr/bin/env python
 
 from distutils.core import setup
